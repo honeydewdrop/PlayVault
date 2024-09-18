b67e9f7dcbb762266ebdb2f28c24666c68990a77
honeydewdrop <smadam26@g.holycross.edu>
2024-09-18 19:41:26 -0400

    fixed hierarchy

--- events/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    # return a simple response
-    return HttpResponse("Welcome to the home page!")
-    
-# Create your views here.
